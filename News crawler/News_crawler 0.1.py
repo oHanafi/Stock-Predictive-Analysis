c22c02bf36324e48a55f6045479099452e84c9c3
oHanafi <ohanafi@outlook.com>
2017-05-25 14:04:56 +0200

    Ophalen nieuws
    Nu kunnen bedrijven opgehaald worden uit de database en kan er meteen
    nieuws op worden gehaald voor deze bedrijven.

--- News crawler/News_crawler 0.1.py
+++ News crawler/News_crawler 0.1.py
@@ -9,42 +9,58 @@ import pyodbc
 
 # List with stocks. This has to be replaced with a database request for version 0.2.
 stockToPull = 'AAPL', 'MSFT','GOOG', 'TSLA', 'AMD', 'INTC','NVDA', 'QCOM', 'NXPI', 'ASML', 'HPQ'
+def connectDatabase():
+        cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost\SQLEXPRESS;DATABASE=ProjectSPA_2;UID=;PWD=')
+        cursor = cnxn.cursor()
+        querryString = "SELECT Short FROM Stock"
+        cursor.execute(querryString)
+        data = cursor.fetchall()        
+        return data
+
+def companyList():
+        companies = []
+        for stock in connectDatabase():
+                stock = str(stock)[int(str(stock).find("'"))+1:]
+                stock = stock[:int(str(stock).find("'"))]
+                companies.append(stock)
+        return companies        
 
 def pullNews(stock):
-    
-    url = "http://www.marketwatch.com/investing/stock/" + stock + "/news"
-    html = urllib.urlopen(url).read()
-    soup = BeautifulSoup(html, "html.parser")
-    
-    listNews = soup.findAll('li', {"class" : "fnewsitem"})
 
-    PolaritySum = 0
-    for links in listNews:
-        link = str(links)[int(str(links).find('/story')):]
-        link = link[:int(str(link).find('"'))]
-
-        # Database has to be checked if link is existing for version 0.2.
-        
-        if link: # Check if link is not empty
-            link = "http://www.marketwatch.com" + link
-            print(link)
-            html = urllib.urlopen(link).read()
+    for stock in companyList():
+            url = "http://www.marketwatch.com/investing/stock/" + stock + "/news"
+            html = urllib.urlopen(url).read()
+            soup = BeautifulSoup(html, "html.parser")
             
-            soup2 = BeautifulSoup(html, "html.parser")
-            for headline_tag in soup2.find_all('h1'):
-                print headline_tag.text
+            listNews = soup.findAll('li', {"class" : "fnewsitem"})
+
+            PolaritySum = 0
+            for links in listNews:
+                link = str(links)[int(str(links).find('/story')):]
+                link = link[:int(str(link).find('"'))]
+
+                # Database has to be checked if link is existing for version 0.2.
                 
-            Content = ""
-            for paragraph_tag in soup2.find_all('p'):
-                #print paragraph_tag.text
-                Content = Content + paragraph_tag.text
+                if link: # Check if link is not empty
+                    link = "http://www.marketwatch.com" + link
+                    print(link)
+                    html = urllib.urlopen(link).read()
+                    
+                    soup2 = BeautifulSoup(html, "html.parser")
+                    for headline_tag in soup2.find_all('h1'):
+                        print headline_tag.text
+                        
+                    Content = ""
+                    for paragraph_tag in soup2.find_all('p'):
+                        #print paragraph_tag.text
+                        Content = Content + paragraph_tag.text
 
-            AnalyseThis = TextBlob(Content)
-            print ("Sentiment: " + str(AnalyseThis.sentiment.polarity))
-            PolaritySum = PolaritySum + AnalyseThis.sentiment.polarity
+                    AnalyseThis = TextBlob(Content)
+                    print ("Sentiment: " + str(AnalyseThis.sentiment.polarity))
+                    PolaritySum = PolaritySum + AnalyseThis.sentiment.polarity
 
-            # The article has to be placed in the database for version 0.2.
-            print ("\n")
+                    # The article has to be placed in the database for version 0.2.
+                    print ("\n")
     print PolaritySum/80
 x = -10
 while x < 0:
