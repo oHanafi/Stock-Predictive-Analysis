17dc38f1242240700ea12c0ee27eb684fd22b529
oHanafi <ohanafi@outlook.com>
2017-05-26 19:11:04 +0200

    Ophalen info om artikel toe te voegen

--- News crawler/News_crawler 0.1.py
+++ News crawler/News_crawler 0.1.py
@@ -45,12 +45,13 @@ def pullNews(stock):
             listNews = soup.findAll('li', {"class" : "fnewsitem"})
             listWriters = soup.findAll('p', {"class" : "source"})
             
-            try:
-                                            
-                    script = re.search('<p class="source">', listWriters[).group(1)
-                    
-            
-            print listWriters
+            listWriters = list(listWriters)
+           
+
+##            for writer in listWriters:
+##                    writer = str(writer)[int(str(writer).find('-'))+2 :]
+##                    writer = writer[:int(str(writer).find('<'))]             
+##                    print(writer)
 
             PolaritySum = 0
             for links in listNews:
@@ -64,11 +65,54 @@ def pullNews(stock):
                     print(link)
                     html = urllib.urlopen(link).read()
                     Title = ""
+                    Author = ""
                     soup2 = BeautifulSoup(html, "html.parser")
                     for headline_tag in soup2.find_all('h1'):
-                        title = headline_tag.text
+                        Title = headline_tag.text
                         print headline_tag.text
-                        
+
+##                    for author_tag in soup2.find_all('module-header'):
+##                        Author = author_tag.text
+##                        print author_tag.text    
+
+                    listWriters = soup2.findAll('h3', {"class" : "module-header"})
+                    Author = ""
+
+                    for module in listWriters:
+                            if str(module).count("href") == 0 and str(module).count("Bulletin") == 0 and str(module).count("References") == 0 and str(module).count("Popular") == 0 and str(module).count("Partner Center") == 0:
+                                    Author = str(module)
+                                    
+                                    break
+                            else:
+                                    continue
+
+                
+                    
+                    
+
+                    
+                    if Author:
+                            Author = Author[int(Author.find('>'))+1:]
+                            Author = Author[:int(Author.find('</b>'))]
+                            Author = " ".join(Author.split())
+                            Author = Author[:Author.index("<")]+ " " + Author[Author.index(">")+1:]
+                            
+                    else:
+                            Author = "Press release"
+                            
+                    if Author == "Press release":
+                            for par in soup2.find_all('p'):
+                                #print paragraph_tag.text
+                                if str(par).count("SOURCE") == 1:                                    
+                                        Author = str(par)[str(par).index("SOURCE")+7:]
+                                        Author = " ".join(Author.split())
+                                        Author = Author[:Author.index("<")]
+                                        break
+
+                                
+                    print Author            
+                                
+                
                     Content = ""
                     for paragraph_tag in soup2.find_all('p'):
                         #print paragraph_tag.text
@@ -78,8 +122,8 @@ def pullNews(stock):
                     Content = Content[:int(Content.find('Join the conversation'))]
                 
                     Content = " ".join(Content.split())
-                    print(Content)
-                    #addToNews(title, Content, 'ja' ,'ja', '2014/12/20 10:12:30')
+                    #print(Content)
+                    #addToNews(Title, Content, 'ja' ,'ja', '2014/12/20 10:12:30')
                     AnalyseThis = TextBlob(Content)
                     print ("Sentiment: " + str(AnalyseThis.sentiment.polarity))
                     PolaritySum = PolaritySum + AnalyseThis.sentiment.polarity
